36b7fd165858101d2f81dcc0c9414fbfddb46684
MarceloCariz <marcejedd300@hotmail.com>
2023-04-17 15:47:16 -0400

    Sincronizacion de hilos

---
+++ src/Hilos/Sincronizacion_hilos/Hilo1.java
@@ -0,0 +1,15 @@
+package Hilos.Sincronizacion_hilos;
+
+public class Hilo1 extends Thread{
+    @Override
+    public void run(){
+        for (int i = 0; i <= 5; i++){
+            System.out.print(i + 1 + ": G");
+            try{
+                Hilo1.sleep(1000);
+            }catch (InterruptedException e){
+                System.out.println("Error en el hilo1 " + e);
+            }
+        }
+    }
+}
