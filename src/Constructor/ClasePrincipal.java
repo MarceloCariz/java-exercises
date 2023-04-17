61ac43396096f369024cde7da33676c21c9018fb
MarceloCariz <marcejedd300@hotmail.com>
2023-04-17 15:58:30 -0400

    Contructor

---
+++ src/Constructor/ClasePrincipal.java
@@ -0,0 +1,11 @@
+package Constructor;
+
+public class ClasePrincipal {
+    public static void main(String[] args) {
+
+        SinConstructor sinConstructor = new SinConstructor();
+//        sinConstructor.PedirNombre();
+//        sinConstructor.Imprimir();
+        ConConstructor conConstructor = new ConConstructor(); // Ejecuta de manera automatica
+    }
+}
---
+++ src/Constructor/ConConstructor.java
@@ -0,0 +1,14 @@
+package Constructor;
+
+import java.util.Scanner;
+
+public class ConConstructor {
+
+    public ConConstructor(){
+        Scanner entrada = new Scanner(System.in);
+        String nombre = "";
+        System.out.println("Cual es tu nombre?");
+        nombre = entrada.nextLine();
+        System.out.println("Tu nombre es: " + nombre);
+    }
+}
